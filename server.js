7b42c20f7bd231894c4deeb7c22bb0dddb701af9
kryvets03 <vadimkrvts03@gmail.com>
2022-01-22 22:45:23 +0200

    Creating modules and routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+//import routes
+const authRoute = require("./routes/auth");
 
 const app = express();
 
@@ -7,17 +11,28 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
 app.post("/name", (req, res) => {
-    if (req.body.name) {
-        return res.json({name: req.body.name});
-    } else {
-        return res.status(400).json({error: "No name provided"});
-    }
+  if (req.body.name) {
+    return res.json({ name: req.body.name });
+  } else {
+    return res.status(400).json({ error: "No name provided" });
+  }
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running in port ${process.env.PORT}`);
-});
+app.use("/api/auth", authRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connect to database!");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running in port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
