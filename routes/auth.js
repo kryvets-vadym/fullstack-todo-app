6dcdccaf3b63d0273aec976518cec0a9679fc408
kryvets03 <vadimkrvts03@gmail.com>
2022-01-24 00:49:59 +0200

    test commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   GET /api/auth/test
+// @desc    Test the auth route
+// @access  Public
 router.get("/test", (req, res) => {
-  res.send("Auth route working!!!");
+    res.send("Auth route working");
 });
 
+// @route   POST /api/auth/register
+// @desc    Create a new user
+// @access  Public
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
-//import routes
-const authRoute = require("./routes/auth");
+// import routes
+// const authRoute = require("./routes/auth");
+
 
 const app = express();
 
@@ -11,28 +12,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Fullstack React Course Express Server");
 });
-
-app.post("/name", (req, res) => {
-  if (req.body.name) {
-    return res.json({ name: req.body.name });
-  } else {
-    return res.status(400).json({ error: "No name provided" });
-  }
-});
-
-app.use("/api/auth", authRoute);
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connect to database!");
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to database");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running in port ${process.env.PORT}`);
+      app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
